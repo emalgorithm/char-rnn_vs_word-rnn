351ed335b6d2feb87517e6381ccf66aba90f2988
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-15 15:53:59 +0000

    Implement logger

---
+++ logger.py
@@ -0,0 +1,18 @@
+import sys
+
+
+class Logger(object):
+    def __init__(self, file_name="logfile.log"):
+        self.terminal = sys.stdout
+        self.log = open(file_name, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        self.terminal.flush()  
+        self.log.flush()
